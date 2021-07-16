16035e97c9cadc57afb5882f5390595adc533d47
temptado <jason1287712@gmail.com>
2021-07-15 21:04:05 -0400

    linked bootstrap to html

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,31 @@ var test = document.querySelector("#test")
 var requestedURL  = "https://api.openweathermap.org/data/2.5/weather?q=burlington&limit=1&appid=bcf6554b28b8c3bcc30e90eb27275f00";
 
 
-function getAPi () {
+// function getAPi () {
 
-    fetch(requestedURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            console.log(data.coord);
-            var longtitudeCoord = data.coord.lon;
-            var latitudeCoord = data.coord.lat;
-            test.textContent = `${longtitudeCoord}, ${latitudeCoord}`;
-            var nextURl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitudeCoord + "&lon=" + longtitudeCoord + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
-            fetch(nextURl)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                console.log(data);
-            })
-        })
+//     fetch(requestedURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             console.log(data);
+//             console.log(data.coord);
+//             var longtitudeCoord = data.coord.lon;
+//             var latitudeCoord = data.coord.lat;
+//             var nextURl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitudeCoord + "&lon=" + longtitudeCoord + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
+//             fetch(nextURl)
+//             .then(function(response) {
+//                 return response.json();
+//             })
+//             .then(function(data) {
+//                 console.log(data);
+//             })
+//         })
         
-}
+// }
 
-getAPi();
+// getAPi();
 
-var momentTest = moment.unix(1626454800).format("DD/MM/YYYY");
+// var momentTest = moment.unix(1626454800).format("DD/MM/YYYY");
 
-console.log(momentTest);
+// console.log(momentTest);
