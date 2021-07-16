feea2145802f56dc3784e4dca93079a32c0e3101
temptado <jason1287712@gmail.com>
2021-07-16 19:51:01 -0400

    made event listener for search button

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,33 @@
-// var test = document.querySelector("#test")
-var requestedURL  = "https://api.openweathermap.org/data/2.5/weather?q=burlington&limit=1&appid=bcf6554b28b8c3bcc30e90eb27275f00";
-
-
-// function getAPi () {
-
-//     fetch(requestedURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             console.log(data);
-//             console.log(data.coord);
-//             var longtitudeCoord = data.coord.lon;
-//             var latitudeCoord = data.coord.lat;
-//             var nextURl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitudeCoord + "&lon=" + longtitudeCoord + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
-//             fetch(nextURl)
-//             .then(function(response) {
-//                 return response.json();
-//             })
-//             .then(function(data) {
-//                 console.log(data);
-//             })
-//         })
-        
-// }
-
-// getAPi();
 
 // var momentTest = moment.unix(1626454800).format("DD/MM/YYYY");
 
 // console.log(momentTest);
+
+var searchButton = document.querySelector("#search-button");
+var cityInput = document.querySelector("#city-input");
+
+
+searchButton.addEventListener("click", function(){
+    var cityChoice = cityInput.value;
+    var cityWeatherURL  = "https://api.openweathermap.org/data/2.5/weather?q=" + cityChoice + "&limit=1&appid=bcf6554b28b8c3bcc30e90eb27275f00";
+    fetch(cityWeatherURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            console.log(data.coord);
+            var longtitudeCoord = data.coord.lon;
+            var latitudeCoord = data.coord.lat;
+            var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitudeCoord + "&lon=" + longtitudeCoord + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
+            fetch(currentWeatherUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            })
+        })
+
+
+})
